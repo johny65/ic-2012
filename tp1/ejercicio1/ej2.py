feacb1f701520e1dbdd2ae96d2204f3380a6805a
Andres <none@none>
2012-08-29 01:54:24 -0300

    genera archivos aletorios con dist circular

--- tp1/ejercicio1/ej2.py
+++ tp1/ejercicio1/ej2.py
@@ -1,24 +1,32 @@
 import math
 import random
 
-def generar_datos(tabla, var, n):
-
+def generar_datos(nombre,var, n):
+	tabla=[]	
+	f = open(str(nombre), "r")
+	for line in f.readlines():
+    		x=[float(s) for s in line.split(',',700)] #lo ideal seria poner la len(linea) pero no me lo toma
+    		x=x[0:len(x)-1]
+    		tabla.append(x)
+	f.close()
+	
 	datos = []
 	cant_por_tabla = n/len(tabla)
 	for i in tabla:
 		for k in range(cant_por_tabla):
 			fila = []
 			for j in i:
-				a = j*(1-var); b = j*(1+var)
-#				if a < b:
-#					d = random.randrange(a, b, int=float)
-#				else:
-#					d = random.randrange(b, a, int=float)
-				d = random.gauss(j, var)
-				fila.append(d)
+				r = random.randrange(0, var, int=float)
+				tita=random.randrange(0,2*math.pi, int=float)
+				d=random.randrange(-1,1, int=float)
+				if d > 0:
+					a=r*math.cos(tita)-j
+				else:
+					a=r*math.sin(tita)-j
+				fila.append(a)
 			datos.append(fila)
 	
-	#completar n!
+	#completar n! hay que ver el resto de n/len(tabla)
 	print datos
 	f = open("salida.txt", 'w')
 	for i in datos:
@@ -28,9 +36,9 @@ def generar_datos(tabla, var, n):
 	f.close()
 
 
-t = [[1, -1], [-1, 1]]
-generar_datos(t, 0.05, 500)
+generar_datos("entradas.txt", 0.05, 500)
 
-
+#Esta funcion genera los datos aletorios con un desvio dado, faltaria agregar a "salidas.txt" los datos 
+#de entradas.txt, y luego si hacer la funcion de las particiones
 		
 
